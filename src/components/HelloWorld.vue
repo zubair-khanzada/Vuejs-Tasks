bfea8f5e7a66b74716d6a6921a4b2856b09747ac
zubai-khanzada <zubairkhanzada7@gmail.com>
2024-03-08 21:27:53 +0500

    Created Vue Counter

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <v-btn color="primary" icon="mdi-plus" size="small" @click="increaseNumber"></v-btn>
+    <span>{{ count }}</span>
+    <v-btn color="primary" icon="mdi-minus" size="small" @click="decreaseNumber"></v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increaseNumber() {
+      this.count++
+    },
+    decreaseNumber() {
+      if (this.count > 0) {
+        this.count--
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+.home {
+  gap: 20px;
+  display: flex;
+  padding: 15px 10px;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
